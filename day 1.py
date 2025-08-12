7267196e8ff9046b0ad59f9817d0c15b02453f29
sshambhavi247 <sshambhavi2007@gmail.com>
2025-08-12 16:14:28 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,24 @@
+print("this is where you type your python  prgm")
+
+#this is single line coomment in python
+
+#let's see variables in python
+
+num = 1000
+pie = 3.14
+city = "Lucknow"
+state = "Uttar Pradesh"
+name = 'Shambhavi'
+multi_line_string = ''' this is multi line comments'''
+sname = "Shambhavi's surname is Srivastava"
+
+
+
+print(num)
+print(pie)
+print(multi_line_string)
+
+#f String also called as formated string
+print(f"I am {name} and I am from {city} in {state}")
+print(sname)
+print("I am", name, "and I am from", city, "in", state)
